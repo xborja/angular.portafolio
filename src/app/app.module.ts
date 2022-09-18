984d8abb6796006fd4c5437fc162c6ca09d8cf7e
Xavier Borja <xavier.borja.m@gmail.com>
2022-09-18 09:23:49 -0500

    Fin de seccion 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Rutas
 import { AppRoutingModule } from './app-routing.module';
+
+//
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';

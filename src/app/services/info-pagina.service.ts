eb4e82023859b5799cf08b9a820531ef9ff9a04b
Xavier Borja <xavier.borja.m@gmail.com>
2022-09-20 05:58:48 -0500

    final de seccion 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,24 +1,40 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { InfoPagina } from '../interfaces/info-pagina.interface';
+import { HttpClient } from '@angular/common/http'
+import { Injectable } from '@angular/core'
+import { InfoPagina } from '../interfaces/info-pagina.interface'
+import { Equipo } from '../interfaces/equipo.interface'
 
 @Injectable({
   providedIn: 'root'
 })
 export class InfoPaginaService {
+  info: InfoPagina = {}
+  equipo: any[] = []
 
-  info: InfoPagina = {};
-  cargada = false;
+  cargada = false
 
-  constructor( private http:HttpClient) { 
-
+  constructor (private http: HttpClient) {
+    this.cargarInfo()
+    this.cargarEquipo()
     // console.log("inicion de servicio de información de pagina");
+  }
+
+  private cargarInfo () {
     //Leer el JSON
-    this.http.get('assets/data/data-pagina.json')
-    .subscribe( (resp:InfoPagina) => {
-      this.cargada = true;
-      this.info = resp;
-      console.log(resp['nombre_corto']);
-    });
+    this.http
+      .get('assets/data/data-pagina.json')
+      .subscribe((resp: InfoPagina) => {
+        this.cargada = true
+        this.info = resp
+      })
+  }
+
+  private cargarEquipo () {
+    //Leer el JSON
+    this.http
+      .get('https://angular-html-89da3-default-rtdb.firebaseio.com/equipo.json')
+      .subscribe((resp: any) => {
+        this.equipo = resp
+        console.log(this.equipo)
+      })
   }
 }
